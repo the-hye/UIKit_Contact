b2cbcafa17cbd56f5bc7f60b70d8695bcda2b3a2
the-hye <ganzi410@gmail.com>
2024-05-21 17:43:27 +0900

    Initialize Project & Delete StoryBoard

---
+++ Contact/Contact/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Contact
+//
+//  Created by JIHYE SEOK on 5/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
